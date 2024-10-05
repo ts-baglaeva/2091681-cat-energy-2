bda38dec101cbb066e4df4e8e776f895ebd44930
ts-baglaeva <165067891+ts-baglaeva@users.noreply.github.com>
2024-10-06 01:50:35 +0500

    Добавляет выпадающее меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navigation__toggle');
+
+navMain.classList.remove('navigation--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('navigation--closed')) {
+    navMain.classList.remove('navigation--closed');
+    navMain.classList.add('navigation--opened');
+  } else {
+    navMain.classList.add('navigation--closed');
+    navMain.classList.remove('navigation--opened');
+  }
+});
