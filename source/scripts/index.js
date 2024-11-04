9a5b069ca72a806239330ad69adf2e72307832a4
ts-baglaeva <165067891+ts-baglaeva@users.noreply.github.com>
2024-11-05 01:29:19 +0500

    Производительность вёрстки (#16)
    * Изменяет ширину iframe
    
    * Готовит проект к проверке
    
    * Вносит исправления после ревью

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -12,31 +12,3 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('navigation--opened');
   }
 });
-
-/*function getTextNodesIn(elem, opt_fnFilter) {
-  let textNodes = [];
-  if (elem) {
-    for (let nodes = elem.childNodes, i = nodes.length; i--;) {
-      const node = nodes[i], nodeType = node.nodeType; if (nodeType == 3) {
-        if (!opt_fnFilter || opt_fnFilter(node, elem)) {
-          if (node.data.trim() !== '') {
-            textNodes.push(node.data.trim() !== '' ? node : '');
-          }
-        }
-      } else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
-        textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
-      }
-    }
-  }
-  return textNodes;
-}
-
-getTextNodesIn(document.body).forEach((item) => {
-  item.textContent = 'У меня есть несколько клиентов, которые записались ко мне на стрижку, не успев даже зайти в салон.';
-});
-
-/*const li = document.querySelectorAll('li');
-li.forEach((item) => {
-  const clone = item.cloneNode(true);
-  item.before(clone);
-});*/
