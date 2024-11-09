b2b0e538d470849ec58043830fed23a7990ffb18
ts-baglaeva <165067891+ts-baglaeva@users.noreply.github.com>
2024-11-09 18:57:11 +0000

    Deploying to gh-pages from @ ts-baglaeva/2091681-cat-energy-2@bed5230e57eb714c6f0844b5e96429d7bed6fe8f 🚀

--- 17/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var n=document.querySelector(".navigation"),a=document.querySelector(".navigation__toggle");n.classList.remove("navigation--nojs");a.addEventListener("click",()=>{n.classList.contains("navigation--closed")?(n.classList.remove("navigation--closed"),n.classList.add("navigation--opened")):(n.classList.add("navigation--closed"),n.classList.remove("navigation--opened"))});
